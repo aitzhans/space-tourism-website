cdda5afa4ee01c82909a930b4588def60faf60d9
Aitzhan <aitzhan@mail.ru>
2021-12-24 21:10:54 +0600

    feat: implement tablet and desktop layout for TechnologyPage

--- src/components/TechPage/TechPage.js
+++ src/components/TechPage/TechPage.js
@@ -10,6 +10,8 @@ import capsuleHor from "./image-space-capsule-landscape.jpg";
 import capsuleVert from "./image-space-capsule-portrait.jpg";
 
 const TechPage = () => {
+  const isDesktop = window.innerWidth > 1024;
+
   const handleCrewClick = (e) => {
     if (e.target.tagName !== "UL") {
       const menuItems = document.querySelectorAll(".tech__item");
@@ -29,45 +31,47 @@ const TechPage = () => {
 
   return (
     <main className="tech__container">
-      <h5 className="inner__subheader">
+      <h5 className="inner__subheader  tech__subheader">
         <span className="inner__subheader--num">03</span>SPACE LAUNCH 101
       </h5>
-      <div className="tech__img-container ">
-        <img
-          className="tech__img  tech__img--visible"
-          src={launchHor}
-          alt="LAUNCH VEHICLE"
-          data-label="launch"
-        />
-        <img
-          className="tech__img"
-          src={spaceportHor}
-          alt="SPACEPORT"
-          data-label="spaceport"
-        />
-        <img
-          className="tech__img"
-          src={capsuleHor}
-          alt="SPACE CAPSULE"
-          data-label="capsule"
-        />
+      <div className="tech__main">
+        <div className="tech__img-container ">
+          <img
+            className="tech__img  tech__img--visible"
+            src={isDesktop ? launchVert : launchHor}
+            alt="LAUNCH VEHICLE"
+            data-label="launch"
+          />
+          <img
+            className="tech__img"
+            src={isDesktop ? spaceportVert : spaceportHor}
+            alt="SPACEPORT"
+            data-label="spaceport"
+          />
+          <img
+            className="tech__img"
+            src={isDesktop ? capsuleVert : capsuleHor}
+            alt="SPACE CAPSULE"
+            data-label="capsule"
+          />
+        </div>
+        <ul className="tech__list" onClick={handleCrewClick}>
+          <Link
+            to="launch"
+            className="tech__item  tech__item--active"
+            data-label="launch"
+          >
+            1
+          </Link>
+          <Link to="spaceport" className="tech__item" data-label="spaceport">
+            2
+          </Link>
+          <Link to="capsule" className="tech__item" data-label="capsule">
+            3
+          </Link>
+        </ul>
+        <Outlet />
       </div>
-      <ul className="tech__list" onClick={handleCrewClick}>
-        <Link
-          to="launch"
-          className="tech__item  tech__item--active"
-          data-label="launch"
-        >
-          1
-        </Link>
-        <Link to="spaceport" className="tech__item" data-label="spaceport">
-          2
-        </Link>
-        <Link to="capsule" className="tech__item" data-label="capsule">
-          3
-        </Link>
-      </ul>
-      <Outlet />
     </main>
   );
 };
